090253c4a71509584894698cd7983e94488f31f6
Fon <1090737707@qq.com>
2017-03-19 01:32:18 +0800

    setting view start

---
+++ public_src/js/personal.js
@@ -0,0 +1,4 @@
+let bodyHeight = document.body.scrollHeight
+let bodyWidth = document.body.scrollWidth
+document.getElementById('body-container').style.height = bodyHeight
+document.getElementById('body-container').style.width = bodyWidth

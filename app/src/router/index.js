63e46ff0aa099d2782c71843a46c43cd91d9f70e
lgybetter <437675103@qq.com>
2017-05-15 21:55:00 +0800

    add the auth checked router and 404 router

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -4,6 +4,7 @@ import auth from '@/views/auth'
 import home from '@/views/home'
 import publish from '@/views/publish'
 import setting from '@/views/setting'
+import notFound from '@/views/not-found'
 import articleList from '@/views/article-list'
 
 Vue.use(Router)
@@ -11,8 +12,21 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/auth',
+      name: 'auth',
+      component: auth
+    },
+    {
       path: '/',
       component: home,
+      beforeEnter: (to, from, next) => {
+        let token = JSON.parse(localStorage.getItem('token'))
+        if (!token) {
+          next('/auth')
+        } else {
+          next()
+        }
+      },
       children: [
         {
           path: '/',
@@ -32,9 +46,9 @@ export default new Router({
       ]
     },
     {
-      path: '/auth',
-      name: 'auth',
-      component: auth
+      path: '*',
+      name: 'notFound',
+      component: notFound
     }
   ]
 })
---
+++ app/src/views/not-found.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>404 not found</div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    setTimeout(() => {
+      this.$router.replace({path: '/'})
+    }, 1000)
+  }
+}
+</script>
+
